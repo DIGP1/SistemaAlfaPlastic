ffdae8a6a8fed9d115ee648e36caf1302f905af1
DIGP1 <diegogarci098@gmail.com>
2024-01-08 21:21:23 -0600

    project uploaded to github

---
+++ src/main/java/com/mycompany/sistemaventas/SistemaVentas.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sistemaventas;
+import forms.*;
+/**
+ *
+ * @author Diego
+ */
+public class SistemaVentas {
+
+    public static void main(String[] args) {
+        Principal prin = new Principal();
+        prin.show(true);
+    }
+}
